be4d112e458cb269196601ac9d395007a2503d9d
Aditya Narayana Choudhury <adityanarayan_choudhury@epam.com>
2025-03-07 15:54:07 +0530

    Changes

--- task01/app/lambdas/hello_world/index.js
+++ task01/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        message: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 };
