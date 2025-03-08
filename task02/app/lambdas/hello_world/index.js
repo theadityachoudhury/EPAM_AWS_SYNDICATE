c8cb502172e990b65d23957f0d41158cb6a1574b
Aditya Narayana Choudhury <adityanarayan_choudhury@epam.com>
2025-03-08 09:39:06 +0530

    Some changes

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,12 +1,12 @@
 exports.handler = async (event) => {
     // TODO implement
     //check the path if it is /hello then return response 200 otherwise 400
-    if (event.path !== "/hello" || event.httpMethod !== "GET") {
+    if (event.path !== "/hello" || event.method !== "GET") {
         return {
             statusCode: 400,
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                message: `Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.httpMethod}`
+                message: `Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.method}`
             }),
         };
     }
