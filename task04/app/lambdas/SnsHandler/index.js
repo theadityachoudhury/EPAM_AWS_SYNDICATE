f31b8fdc21b5bf10cdef8bf9de302458b2fff826
Aditya Narayana Choudhury <adityanarayan_choudhury@epam.com>
2025-03-13 12:42:28 +0530

    Task04

--- task04/app/lambdas/SnsHandler/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    // TODO implement
-    console.log('Received event:', JSON.stringify(event));
-    return {
-        statusCode: 200,
-        body: 'Processed SNS Messages Successfully'
-      };
-};
